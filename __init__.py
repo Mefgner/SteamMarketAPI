3bf4c69c5f5537c7511b7a75dcf29dc7a229bb29
Mefgner <evgeniyprasol1996@gmail.com>
2022-07-14 16:18:09 +0300

    Update

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from .smapi import SteamMarketHandler
 __all__ = ['SteamMarketHandler']
 
 __name__ = 'SteamMarketAPI'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __doc__ = """Steam Market API provides you to use Community Market of CSGO game easily and automatize it."""
